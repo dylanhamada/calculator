4f4b5235777e87146f5232c4a914e8d311343847
Dylan Hamada <dylan.hamada@gmail.com>
2019-07-30 15:33:41 -0400

    Updates

---
+++ precache-manifest.b99c2d2a00c47565484d7f9d7260cc87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be9ce29d9831e5df9dac33434c2e785c",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "dd345b9e6415ddd8c48c",
+    "url": "/calculator/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/calculator/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "dd345b9e6415ddd8c48c",
+    "url": "/calculator/static/js/main.bb30d2ea.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
