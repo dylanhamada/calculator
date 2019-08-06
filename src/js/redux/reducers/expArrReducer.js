703509a7b1699ac80c21c591d6f5107e198ecfa1
Dylan Hamada <dylan.hamada@gmail.com>
2019-08-05 21:09:39 -0400

    Tweak reducers to correctly display user inputs

--- src/js/redux/reducers/expArrReducer.js
+++ src/js/redux/reducers/expArrReducer.js
@@ -3,15 +3,13 @@ import { defaultState } from "./rootReducer";
 /* Updates store by pushing current number into expressions array,
   then clearing the current number */
 const expArrReducer = (state = defaultState, action) => {
-  let num = state.currentNum.join("");
   let func = action.func;
   let expArr = state.expressionArr;
 
   switch (action.type) {
     case "ADD_EXP_ARR":
-      console.log(num);
       return Object.assign({}, state, {
-        expressionArr: [...expArr, num, func],
+        expressionArr: [...expArr, func],
         currentNum: [func]
       });
     default:
--- src/js/redux/reducers/numReducer.js
+++ src/js/redux/reducers/numReducer.js
@@ -3,6 +3,7 @@ import { defaultState } from "./rootReducer";
 /* Updates store value/expression with user-input number */
 const numReducer = (state = defaultState, action) => {
   let num = state.currentNum;
+  let exp = state.expressionArr;
 
   switch (action.type) {
     case "ADD_NUM":
@@ -14,23 +15,34 @@ const numReducer = (state = defaultState, action) => {
         num = [];
       }
 
-      console.log(state.expressionArr);
-
+      /* Adds user-input number to currentNum and expressionArr, providing
+      a value for Display and DisplayExp components to display  */
       return typeof num[0] === "string"
         ? Object.assign({}, state, {
-            currentNum: [action.num]
+            currentNum: [action.num],
+            expressionArr: [...exp, action.num]
           })
         : Object.assign({}, state, {
-            currentNum: [...num, action.num]
+            currentNum: [...num, action.num],
+            expressionArr: [...exp, action.num]
           });
     /* Updates store value/expression with a decimal */
     case "ADD_DEC":
       if (!num.includes(".")) {
-        return Object.assign({}, state, {
-          currentNum: [...num, "."]
-        });
+        /* If decimal comes after an operator, currentNum is cleared
+        and expressionArr includes a zero before the decimal */
+        if (typeof num[0] === "string") {
+          return Object.assign({}, state, {
+            currentNum: [0, "."],
+            expressionArr: [...exp, 0, "."]
+          });
+        } else {
+          return Object.assign({}, state, {
+            currentNum: [...num, "."],
+            expressionArr: [...exp, "."]
+          });
+        }
       }
-      break;
     default:
       return state;
   }
